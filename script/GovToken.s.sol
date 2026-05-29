6d309b296fe7cee123b8682409d1d9a44d9f7c84
keysoz <keysoz122@gmail.com>
2026-05-29 16:48:29 +0300

    test: add basic Script to deploy the contract

---
+++ script/GovToken.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.20;
+
+import {Script} from "forge-std/Script.sol";
+import {GovernX} from "../src/GovToken.sol";
+
+contract DeployGovernX is Script {
+    string name = "Governor X Token";
+    string symbol = "GVX";
+    GovernX governor;
+
+    function run() external returns (GovernX) {
+        vm.startBroadcast();
+        governor = new GovernX(name, symbol);
+        vm.stopBroadcast();
+        return governor;
+    }
+}
